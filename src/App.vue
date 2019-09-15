83008edb965b59862a3609374857bca283b14984
codeatmordor <gaurav.bhu3790@gmail.com>
2019-09-15 14:15:52 +0530

    First Commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="main-app" class="container">
+    <h4>{{title}}</h4>
+    <font-awesome-icon icon="plus" class="mr-2"/> Add Appointment
+    <div v-for="(item,i) in appointments" v-bind:key="i"> 
+      <h4>{{item.petName}}</h4>
+      <p>{{item.aptNotes}}</p>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome"
+
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  name: 'MainApp',
+  data: function(){
+    return {
+      title : "Appointment List",
+      appointments:[{"petName":"Spot","petOwner":"Constance Smith","aptDate":"2017-07-24 08:30","aptNotes":"This German Shepherd is having some back pain"},{"petName":"Goldie","petOwner":"Barot Bellingham","aptDate":"2017-07-22 15:50","aptNotes":"This Goldfish has some weird spots in the belly"},{"petName":"Mitten","petOwner":"Hillary Goldwyn","aptDate":"2017-07-21 9:15","aptNotes":"Cat has excessive hairballs"},{"petName":"Buffy","petOwner":"Hassum Harrod","aptDate":"2017-07-20 15:30","aptNotes":"This Chihuahua has not eaten for three days and is lethargic"}]
+    }
+  },
+  components:{
+    FontAwesomeIcon
   }
+
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
