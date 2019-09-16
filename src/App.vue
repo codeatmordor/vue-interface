c1d8c18e11cc9ee23ae7b23fedfae7a849b7faf8
codeatmordor <gaurav.bhu3790@gmail.com>
2019-09-16 11:27:05 +0530

    Add and Search for appointments

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,97 @@
 <template>
   <div id="main-app" class="container">
-  <div class="row justify-content-center">
-   <appointment-list v-bind:appointments="appointments" @remove="removeItem"/>
-   </div>
+    <div class="row justify-content-center">
+      <add-appointment @add="addItem"/>
+      <search-appointments
+        @searchRecords="searchAppointments"
+        :myKey="filterKey"
+        :myDir="filterDir"
+        @requestKey="changeKey"
+        @requestDir="changeDir"
+      />
+      <appointment-list :appointments="filteredApts" @remove="removeItem" @edit="editItem"/>
+    </div>
   </div>
 </template>
 
 <script>
-
-import AppointmentList from "./components/AppointmentList"
-import _ from "lodash"
-import axios from "axios"
-
+import AddAppointment from "./components/AddAppointment";
+import SearchAppointments from "./components/SearchAppointments";
+import AppointmentList from "./components/AppointmentList";
+import _ from "lodash";
+import axios from "axios";
 
 export default {
-  name: 'MainApp',
-  data: function(){
+  name: "MainApp",
+  data: function() {
     return {
-      appointments:[]
+      appointments: [],
+      filterKey: "petName",
+      filterDir: "asc",
+      searchTerms: "",
+      aptIndex: 0
+    };
+  },
+  components: {
+    AppointmentList,
+    SearchAppointments,
+    AddAppointment
+  },
+  mounted() {
+    axios.get("./data/appointments.json").then(
+      response =>
+        (this.appointments = response.data.map(item => {
+          item.aptId = this.aptIndex;
+          this.aptIndex++;
+          return item;
+        }))
+    );
+  },
+  computed: {
+    searchedApts: function() {
+      return this.appointments.filter(item => {
+        return (
+          item.petName.toLowerCase().match(this.searchTerms.toLowerCase()) ||
+          item.petOwner.toLowerCase().match(this.searchTerms.toLowerCase()) ||
+          item.aptNotes.toLowerCase().match(this.searchTerms.toLowerCase())
+        );
+      });
+    },
+    filteredApts: function() {
+      return _.orderBy(
+        this.searchedApts,
+        item => {
+          return item[this.filterKey].toLowerCase();
+        },
+        this.filterDir
+      );
     }
   },
-  components:{
-    AppointmentList
-  },
-  mounted(){
-    axios.get("./data/appointments.json").then(response=>(this.appointments=response.data));
-  },
-  methods:{
-    removeItem:function(apt){
-      this.appointments = _.without(this.appointments,apt);
+  methods: {
+    changeKey: function(value) {
+      this.filterKey = value;
+    },
+    changeDir: function(value) {
+      this.filterDir = value;
+    },
+    searchAppointments: function(terms) {
+      this.searchTerms = terms;
+    },
+    addItem: function(apt) {
+      apt.aptId = this.aptIndex;
+      this.aptIndex++;
+      this.appointments.push(apt);
+    },
+    removeItem: function(apt) {
+      this.appointments = _.without(this.appointments, apt);
+    },
+    editItem: function(id, field, text) {
+      const aptIndex = _.findIndex(this.appointments, {
+        aptId: id
+      });
+      this.appointments[aptIndex][field] = text;
     }
-  } 
-
-}
+  }
+};
 </script>
 
